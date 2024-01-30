0dcafce649eb5f749c59c8129dde57c690dc5386
Carlos Marcelo Castillo <carlos.9062@alumnos.udg.mx>
2024-01-30 02:22:50 -0600

    Actual working version

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 
 
-reset();
 
-let tictactoeElement = [
+const tictactoeElement = [
     [document.querySelector('.js-00'),document.querySelector('.js-01'),document.querySelector('.js-02')],
     [document.querySelector('.js-10'),document.querySelector('.js-11'),document.querySelector('.js-12')],
     [document.querySelector('.js-20'),document.querySelector('.js-21'),document.querySelector('.js-22')]
 ];
-let tictactoe = array(3).fill(array(3).fill(false))
 
+const resultElement = document.querySelector('.js-result')
+
+let tictactoe = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+
+
+
+let winner = '';
+
+reset();
 console.log(tictactoeElement);
+console.log(tictactoe);
 
 function reset(){
     for(let i = 0; i < 3; i++){
@@ -17,34 +29,119 @@ function reset(){
             document.querySelector(".js-"+String(i)+String(j)).src = "images/Blank.png";
         }
     }
+    tictactoe = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ]
+    winner = '';
+    resultElement.textContent = winner;
 }
 
 function play(row, column){
-    let temp = document.querySelector(".js-"+String(row)+String(column)).src = "images/Blank.png";
+    if(winner != ''){
+        return
+    }
+    if(!tictactoe[row][column]){
+        tictactoe[row][column] = 1;
+        tictactoeElement[row][column].src='images/Circle.png';
 
-
-   /*  if (temp.src === "http://127.0.0.1:5500/images/Blank.png"){
-        temp.src="images/Circle.png"
+        check();
         computer();
-    } */
+        check();
+    }
+
 }
 
 
 function computer(){
+    if(winner != ''){
+        return
+    }
 
+    let count = 0;
     while(true){
-
+        count++;
+        if(count > 50){
+            break;
+        }
         row = Math.floor(Math.random()*3);
         column = Math.floor(Math.random()*3);
     
-        if(tictactoeElement[row][column].src=== 'http://127.0.0.1:5500/images/Blank.png'){
+        if(!tictactoe[row][column]){
+            tictactoe[row][column] = -1;
             tictactoeElement[row][column].src = 'images/Cross.png';
+            console.log(tictactoe);
             return
         }
-
     }
-
-        
+    for(let i = 0; i < 3; i++){
+        for(let j = 0; j < 3; j++){
+            if(!tictactoe[row][column]){
+                tictactoe[row][column] = -1;
+                tictactoeElement[row][column].src = 'images/Cross.png';
+                console.log(tictactoe);
+                return
+            }
+        }
+    }
 }
 
+function check(){
 
+    //console.log(tictactoe);
+
+    //console.log("we're adding " + tictactoe[0][0] + " with " + tictactoe[0][1] + " with " + tictactoe[0][2] + " which gives us = " + String(Number(tictactoe[0][0])+Number(tictactoe[0][1])+Number(tictactoe[0][2])))
+
+    if(winner != ''){
+        return;
+    }
+
+    let end = true;
+    let temp = 0;
+    for(let i = 0; i < 3; i++){
+        temp = tictactoe[i][0]+tictactoe[i][1]+tictactoe[i][2];
+        if(temp===3){
+            winner = 'You win';
+        }
+        if(temp===-3){
+            winner = 'You lose';
+        }
+        temp = tictactoe[0][i]+tictactoe[1][i]+tictactoe[2][i];
+        if(temp===3){
+            winner = 'You win';
+        }
+        if(temp===-3){
+            winner = 'You lose';
+        }
+        for(let j = 0; j < 3; j++){
+            if(tictactoe[i][j] == 0){
+                end = false
+            }
+        }
+    }
+    temp = tictactoe[0][0]+tictactoe[1][1]+tictactoe[2][2];
+    if(temp===3){
+        winner = 'You win';
+    }
+    if(temp===-3){
+        winner = 'You lose';
+    }
+    temp = tictactoe[2][0]+tictactoe[1][1]+tictactoe[0][2];
+    if(temp===3){
+        winner = 'You win';
+    }
+    if(temp===-3){
+        winner = 'You lose';
+    }
+    
+    if(end && winner == ''){
+        winner = 'Draw'
+    }
+    
+    if(winner != ''){
+        resultElement.textContent = winner;
+    }
+
+
+}
